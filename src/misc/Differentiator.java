6483ef876a1f35a0090809d7bfee52f0e865104a
Igor Wiedler <igor@wiedler.ch>
2010-03-30 23:23:51 +0200

    use iteration in differentiator

--- src/misc/Differentiator.java
+++ src/misc/Differentiator.java
@@ -1,18 +1,17 @@
 package misc;
 
+import java.util.Map.Entry;
+
 public class Differentiator {
 	static public PolyFunction differentiate(PolyFunction f) {
 		PolyFunction a = new PolyFunction();
 		
-		for (int i = 0, size = f.getMaxGrade(); i <= size; i++) {
-			if (!f.hasKoeff(i)) {
-				continue;
-			}
-			if (i == 0) {
+		for (Entry<Integer, Double> entry : f) {
+			if (entry.getKey() == 0) {
 				// x^0 => 0
 				continue;
 			}
-			a.setKoeff(i-1, i * f.getKoeff(i));
+			a.setKoeff(entry.getKey()-1, entry.getKey() * entry.getValue());
 		}
 		
 		return a;
