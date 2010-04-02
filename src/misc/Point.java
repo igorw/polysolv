8d12c4f299e6c3915177d11e201937e00f2da16e
Igor Wiedler <igor@wiedler.ch>
2010-04-03 00:23:58 +0200

    make point equals asserteable

--- src/misc/Point.java
+++ src/misc/Point.java
@@ -1,5 +1,6 @@
 package misc;
 
+
 // represents one point on a graph (x/y)
 public class Point {
 	private Double x = 0.0;
@@ -25,5 +26,21 @@ public class Point {
 	}
 	public Double getY() {
 		return y;
+	}
+	
+	@Override
+	public boolean equals(Object o) {
+		return hashCode() == o.hashCode();
+	}
+	
+	@Override
+	public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+
+        result = prime * result + x.hashCode();
+        result = prime * result + y.hashCode();
+        
+        return result;
 	}
 }
