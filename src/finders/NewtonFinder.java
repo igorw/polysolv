f735f505d09d67dd7fce441894d5fcc556c10b3f
Igor Wiedler <igor@wiedler.ch>
2010-04-02 22:03:30 +0200

    rewrite large parts of NewtonFinder

--- src/finders/NewtonFinder.java
+++ src/finders/NewtonFinder.java
@@ -4,125 +4,138 @@ import java.util.Collections;
 import java.util.Vector;
 
 import misc.Differentiator;
+import misc.Point;
 import misc.PolyFunction;
 
 // y = ax^3 + bx^2 + cx + d
 // mšgliche nullstellen 1-3
 public class NewtonFinder implements FinderInterface {
-	// constants
-	public static final int SEARCH_UNKNOWN = 0;
-	public static final int SEARCH_LEFT = 1;
-	public static final int SEARCH_RIGHT = 2;
 	
 	// precision/depth of recursive newton algorithm
-	private int newtonDepth = 10;
+	private int newtonDepth = 1000;
+	
+	// offset for searching left/right
+	private int newtonOffset = 100;
+	
+	private double a, b, c, d;
 	
 	public Vector<Double> find(PolyFunction f) throws InvalidFuncException {
 		Vector<Double> results = new Vector<Double>();
 		
-		double a = f.getKoeff(3);
-		double b = f.getKoeff(2);
-		double c = f.getKoeff(1);
-		double d = f.getKoeff(0);
+		a = f.getKoeff(3);
+		b = f.getKoeff(2);
+		c = f.getKoeff(1);
+		d = f.getKoeff(0);
 		
 		// ableiten damit sie quadratisch ist
-		PolyFunction fa = Differentiator.differentiate(f);
-		
 		// nullstellen x koordinaten der abgeleiteten funktion
 		// entsprechen den extrema von f
-		Vector<Double> fax = new QuadricFinder().find(fa);
+		PolyFunction fa = Differentiator.differentiate(f);
+		Vector<Double> extrema = new QuadricFinder().find(fa);
 		
-		// extrema aufsteigend sortieren
-		Collections.sort(fax);
+		Vector<ContextAwarePoint> points = preparePoints(f, extrema);
 		
-		if (fax.size() == 1) {
+		if (points.size() == 1) {
 			// nur ein extremum
-			// => sattelpunkt
-			// => nur eine nullstelle
+			ContextAwarePoint p = points.firstElement();
 			
-			double x1 = fax.firstElement();
-			
-			if (f.calculate(x1) == 0.0) {
-				// extremum = nullstelle
-				results.add(x1);
-			} else {
-				int searchWhere = SEARCH_UNKNOWN;
-				
-				// von positivem polynom 3ten grades ausgehen
-				// von unten links nach oben rechts
-				if (f.calculate(x1) > 0.0) {
-					// extremum Ÿber x-achse
-					// => links suchen
-					searchWhere = SEARCH_LEFT;
-				} else /*if (f.calculate(x1) < 0.0)*/ {
-					// extremum unter x-achse
-					// => rechts suchen
-					searchWhere = SEARCH_RIGHT;
-				}
-				
-				if (a < 0.0) {
-					// negatives polynom 3ten grades
-					// von oben links nach unten rechts
-					// searchWhere muss invertiert werden
-					searchWhere = (searchWhere == SEARCH_LEFT) ? SEARCH_RIGHT : SEARCH_LEFT;
-				}
-				
-				// do the searching
-				// search either left or right
-				results.add(newton(f, x1 + (searchWhere == SEARCH_LEFT ? -1 : 1), newtonDepth));
+			if (p.isRoot()) {
+				results.add(p.getPoint().getX());
+			} else if (p.hasLeftRoot()) {
+				results.add(newton(f, p.getPoint().getX() - 1, newtonDepth));
+			} else if (p.hasRightRoot()) {
+				results.add(newton(f, p.getPoint().getX() + 1, newtonDepth));
 			}
 		} else {
-			if (a > 0.0) {
-				// positives polynom 3ten grades
-				// von unten links nach oben rechts
+			// mehr als ein extremum
+			ContextAwarePoint firstPoint = points.firstElement();
+			if (firstPoint.hasLeftRoot()) {
+				results.add(newton(f, firstPoint.getPoint().getX() - newtonOffset, newtonDepth));
+			}
+			
+			ContextAwarePoint prev = null;
+			for (ContextAwarePoint current : points) {
+				if (current.isRoot()) {
+					results.add(current.getPoint().getX());
+				}
 				
-				Double prev = null;
-				for (Double x : fax) {
-					// iterate over all extrema
-					
-					if (f.calculate(x) == 0.0) {
-						// extremum = nullstelle
-						results.add(x);
-						
-						prev = x;
-						continue;
-					}
-					
-					if (prev == null) {
-						// first iteration
-						if (f.calculate(x) > 0.0) {
-							// Ÿber x-achse
-							// links suchen
-							results.add(newton(f, x - 1, newtonDepth));
-						}
-						
-						prev = x;
-						continue;
-					}
-					
-					// calculate coordinates of prev and current
-					double y1 = f.calculate(prev);
-					double y2 = f.calculate(x);
-					
-					// vorzeichen vergleichen
-					// falls es Šndert ist nullstelle dazwischen
-					// mit mittelwert suchen
-					if (y1 < 0 && y2 > 0 || y1 > 0 && y2 < 0) {
-						results.add(newton(f, (prev + x) / 2, newtonDepth));
-					}
-					
-					prev = x;
+				if (prev == null) {
+					prev = current;
+					continue;
 				}
-				// after iteration
-				if (f.calculate(prev) < 0.0) {
-					// unter x-achse
-					// rechts suchen
-					results.add(newton(f, prev + 1, newtonDepth));
+				
+				if (prev.hasRightRoot() && current.hasLeftRoot()) {
+					results.add(newton(f, (prev.getPoint().getX()+prev.getPoint().getX()) / 2.0, newtonDepth));
 				}
+				
+				prev = current;
+			}
+			
+			ContextAwarePoint lastPoint = points.lastElement();
+			if (lastPoint.hasRightRoot()) {
+				results.add(newton(f, firstPoint.getPoint().getX() + newtonOffset, newtonDepth));
 			}
 		}
 		
 		return results;
+	}
+	
+	public Vector<ContextAwarePoint> preparePoints(PolyFunction f, Vector<Double> extrema) {
+		// extrema aufsteigend sortieren
+		Collections.sort(extrema);
+		
+		// vektor von ContextAwarePoints
+		Vector<ContextAwarePoint> points = new Vector<ContextAwarePoint>();
+		
+		ContextAwarePoint prev = null;
+		for (Double x : extrema) {
+			// iterate over all extrema
+			
+			ContextAwarePoint current = new ContextAwarePoint(x, f);
+			points.add(current);
+			
+			if (f.calculate(x) == 0.0) {
+				// extremum = nullstelle
+				current.setRoot(true);
+			}
+			
+			if (prev == null) {
+				prev = current;
+				continue;
+			}
+			
+			// calculate coordinates of prev and current
+			double y1 = prev.getPoint().getY();
+			double y2 = current.getPoint().getY();
+			
+			// vorzeichen vergleichen
+			// falls es Šndert ist nullstelle dazwischen
+			// mit mittelwert suchen
+			if (y1 < 0 && y2 > 0 || y1 > 0 && y2 < 0) {
+				prev.setRightRoot(true);
+				current.setLeftRoot(true);
+			}
+			
+			prev = current;
+		}
+		
+		ContextAwarePoint firstPoint = points.firstElement();
+		if (a > 0.0 && firstPoint.getPoint().getY() > 0.0 ||
+			a < 0.0 && firstPoint.getPoint().getY() < 0.0) {
+			// Ÿber x-achse
+			// links suchen
+			firstPoint.setLeftRoot(true);
+		}
+
+		ContextAwarePoint lastPoint = points.lastElement();
+		if (a > 0.0 && lastPoint.getPoint().getY() < 0.0 ||
+			a < 0.0 && lastPoint.getPoint().getY() > 0.0) {
+			// unter x-achse
+			// rechts suchen
+			lastPoint.setRightRoot(true);
+		}
+		
+		return points;
 	}
 	
 	// recursive newton
@@ -135,6 +148,44 @@ public class NewtonFinder implements FinderInterface {
 			return newton(f, x, --depth);
 		} else {
 			return x;
+		}
+	}
+	
+	// a point that knows where roots (nullstellen) are
+	private class ContextAwarePoint {
+		private Point point = new Point();
+		private PolyFunction f;
+		
+		// where are the roots
+		private boolean isRoot = false,
+						leftRoot = false,
+						rightRoot = false;
+		
+		public ContextAwarePoint(Double x, PolyFunction f) {
+			this.f = f;
+			point.setX(x);
+			point.setY(f.calculate(x));
+		}
+		public Point getPoint() {
+			return point;
+		}
+		public void setRoot(boolean isRoot) {
+			this.isRoot = isRoot;
+		}
+		public boolean isRoot() {
+			return isRoot;
+		}
+		public void setLeftRoot(boolean leftRoot) {
+			this.leftRoot = leftRoot;
+		}
+		public boolean hasLeftRoot() {
+			return leftRoot;
+		}
+		public void setRightRoot(boolean rightRoot) {
+			this.rightRoot = rightRoot;
+		}
+		public boolean hasRightRoot() {
+			return rightRoot;
 		}
 	}
 }
