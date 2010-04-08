3f49ff21db2ab5fa3e168963ce6e95c885add755
Igor Wiedler <igor@wiedler.ch>
2010-04-08 15:37:23 +0200

    clear out crap, new reto implementation

--- src/finders/NewtonFinder.java
+++ src/finders/NewtonFinder.java
@@ -1,14 +1,11 @@
 package finders;
 
+import java.text.DecimalFormat;
 import java.util.Collections;
 import java.util.Vector;
 
-import misc.Apex;
-import misc.Bisect;
 import misc.Differentiate;
-import misc.Point;
 import misc.PolyFunction;
-import misc.Range;
 
 // y = ax^3 + bx^2 + cx + d
 // mögliche nullstellen 1-3
@@ -16,12 +13,6 @@ public class NewtonFinder implements FinderInterface {
 	
 	// precision/depth of recursive newton algorithm
 	private int newtonDepth = 1000;
-	
-	// offset for searching left/right
-	private int newtonOffset = 100;
-	
-	// amount of range bisect iterations
-	private int bisectDepth = 10;
 	
 	private double a/*, b, c, d*/;
 	
@@ -51,172 +42,57 @@ public class NewtonFinder implements FinderInterface {
 			System.out.println(extrema);
 		}
 		
-		Vector<ContextAwarePoint> points = preparePoints(f, extrema);
-		
-		if (points.size() == 0) {
-			// keine nullstelle der ableitung gefunden
-			// scheitelpunkt der ableitung entspricht sattelpunkt
-			results.add(Apex.apex(fa).getX());
+		// ableitung hat keine nullstelle
+		// an irgendeinem ort suchen
+		// f hat nur eine nullstelle
+		if (extrema.size() == 0 || extrema.size() == 1) {
+			results.add(round(newton(f, 1.0, newtonDepth)));
 			return results;
 		}
 		
-		if (points.size() == 1) {
-			// nur ein extremum
-			ContextAwarePoint p = points.firstElement();
-			
-			if (p.isRoot()) {
-				results.add(p.getPoint().getX());
-			} else if (p.hasLeftRoot()) {
-				results.add(newton(f, p.getPoint().getX() - 1, newtonDepth));
-			} else if (p.hasRightRoot()) {
-				results.add(newton(f, p.getPoint().getX() + 1, newtonDepth));
-			}
-		} else {
-			// mehr als ein extremum
-			ContextAwarePoint firstPoint = points.firstElement();
-			if (firstPoint.hasLeftRoot()) {
-				try {
-					Range range = findRangeLeft(firstPoint.getPoint().getX(), f);
-					range = new Bisect().bisect(range, f, bisectDepth);
-					System.out.println(range);
-				} catch (RangeNotFoundException e) {
-					e.printStackTrace();
-				}
-				results.add(newton(f, firstPoint.getPoint().getX() - newtonOffset, newtonDepth));
-			}
-			
-			ContextAwarePoint prev = null;
-			for (ContextAwarePoint current : points) {
-				if (current.isRoot()) {
-					results.add(current.getPoint().getX());
-				}
-				
-				if (prev == null) {
-					prev = current;
-					continue;
-				}
-				
-				if (prev.hasRightRoot() && current.hasLeftRoot()) {
-					results.add(newton(f, (prev.getPoint().getX()+prev.getPoint().getX()) / 2.0, newtonDepth));
-				}
-				
-				prev = current;
-			}
-			
-			ContextAwarePoint lastPoint = points.lastElement();
-			if (lastPoint.hasRightRoot()) {
-				results.add(newton(f, firstPoint.getPoint().getX() + newtonOffset, newtonDepth));
-			}
-		}
-		
-		return results;
-	}
-
-	// make x coordinates aware of roots
-	// in certain cases it can be empty
-	// in which case there is a saddle point
-	// this case must be handled by the caller
-	public Vector<ContextAwarePoint> preparePoints(PolyFunction f, Vector<Double> extrema) {
 		// extrema aufsteigend sortieren
 		Collections.sort(extrema);
 		
-		// vektor von ContextAwarePoints
-		Vector<ContextAwarePoint> points = new Vector<ContextAwarePoint>();
-		
-		ContextAwarePoint prev = null;
-		for (Double x : extrema) {
-			// iterate over all extrema
-			
-			ContextAwarePoint current = new ContextAwarePoint(x, f);
-			points.add(current);
-			
-			if (f.calculate(x) == 0.0) {
-				// extremum = nullstelle
-				current.setRoot(true);
-			}
-			
-			if (prev == null) {
-				prev = current;
-				continue;
-			}
-			
-			// calculate coordinates of prev and current
-			double y1 = prev.getPoint().getY();
-			double y2 = current.getPoint().getY();
-			
-			// vorzeichen vergleichen
-			// falls es ändert ist nullstelle dazwischen
-			// mit mittelwert suchen
-			if (y1 < 0 && y2 > 0 || y1 > 0 && y2 < 0) {
-				prev.setRightRoot(true);
-				current.setLeftRoot(true);
-			}
-			
-			prev = current;
+		// positive 3ten grades
+		// erstes extremum über null
+		// ODER
+		// negative 3ten grades
+		// zweites extremum unter null
+		//
+		// => links suchen
+		Double firstElement = extrema.firstElement();
+		if (a > 0.0 && firstElement > 0.0 ||
+			a < 0.0 && firstElement < 0.0) {
+			results.add(round(newton(f, firstElement - 1, newtonDepth)));
 		}
 		
-		if (points.size() > 0) {
-			ContextAwarePoint firstPoint = points.firstElement();
-			if (a > 0.0 && firstPoint.getPoint().getY() > 0.0 ||
-				a < 0.0 && firstPoint.getPoint().getY() < 0.0) {
-				// über x-achse
-				// links suchen
-				firstPoint.setLeftRoot(true);
-			}
-
-			ContextAwarePoint lastPoint = points.lastElement();
-			if (a > 0.0 && lastPoint.getPoint().getY() < 0.0 ||
-				a < 0.0 && lastPoint.getPoint().getY() > 0.0) {
-				// unter x-achse
-				// rechts suchen
-				lastPoint.setRightRoot(true);
+		// von erster bis vorletzer nullstelle
+		// immer mit nächster vergleichen
+		for (int i = 0; i < extrema.size() - 1; i++) {
+			Double x1 = extrema.get(i);
+			Double x2 = extrema.get(i + 1);
+			
+			// vorzeichen wechsel
+			// nullstelle suchen
+			if (signChange(f.calculate(x1), f.calculate(x2))) {
+				results.add(round(newton(f, (x1 + x2) / 2.0, newtonDepth)));
 			}
 		}
 		
-		return points;
-	}
-	
-	// find a range on the left side
-	// @todo the same for right and in between
-	public Range findRangeLeft(Double x, PolyFunction f) throws RangeNotFoundException {
-		Double prev = null, prevResult = null;
-		
-		// hard-coded depth
-		for (int i = 0; i < 10; i++) {
-			// current x
-			double current = x - Math.pow(10, i);
-			
-			// current y
-			double result = f.calculate(current);
-			
-			// first iteration
-			if (prev == null) {
-				prev = current;
-				prevResult = result;
-				continue;
-			}
-			
-			// arithmetic sign change
-			// we have our range
-			if (prevResult < 0.0 && result > 0.0 ||
-				prevResult > 0.0 && result < 0.0) {
-				// return range
-				return new Range(prev, current);
-			}
-			
-			// special case
-			// we actually found the root
-			// return something close
-			if (result == 0.0) {
-				return new Range(prev, current - 1.0);
-			}
-			
-			prev = current;
-			prevResult = result;
+		// positive 3ten grades
+		// erstes extremum unter null
+		// ODER
+		// negative 3ten grades
+		// zweites extremum über null
+		//
+		// => rechts suchen
+		Double lastElement = extrema.lastElement();
+		if (a > 0.0 && lastElement < 0.0 ||
+			a < 0.0 && lastElement > 0.0) {
+			results.add(round(newton(f, lastElement + 1, newtonDepth)));
 		}
 		
-		// we did not find a good range
-		throw new RangeNotFoundException();
+		return results;
 	}
 	
 	// recursive newton
@@ -232,53 +108,15 @@ public class NewtonFinder implements FinderInterface {
 		}
 	}
 	
+	// auf 3 stellen runden
+	public Double round(Double value) {
+		DecimalFormat twoDForm = new DecimalFormat("#.###");
+		return Double.valueOf(twoDForm.format(value));
+	}
+	
 	// helper function to check if sign changed
 	static public boolean signChange(Double y1, Double y2) {
 		return (y1 < 0 && y2 > 0 ||
 				y1 > 0 && y2 < 0);
-	}
-	
-	// a point that knows where roots (nullstellen) are
-	private class ContextAwarePoint {
-		private Point point = new Point();
-		
-		// where are the roots
-		private boolean isRoot = false,
-						leftRoot = false,
-						rightRoot = false;
-		
-		public ContextAwarePoint(Double x, PolyFunction f) {
-			point.setX(x);
-			point.setY(f.calculate(x));
-		}
-		public Point getPoint() {
-			return point;
-		}
-		public void setRoot(boolean isRoot) {
-			this.isRoot = isRoot;
-		}
-		public boolean isRoot() {
-			return isRoot;
-		}
-		public void setLeftRoot(boolean leftRoot) {
-			this.leftRoot = leftRoot;
-		}
-		public boolean hasLeftRoot() {
-			return leftRoot;
-		}
-		public void setRightRoot(boolean rightRoot) {
-			this.rightRoot = rightRoot;
-		}
-		public boolean hasRightRoot() {
-			return rightRoot;
-		}
-		
-		@Override
-		public String toString() {
-			return point.toString() + " "
-				+ (isRoot() ? "h" : "")
-				+ (hasLeftRoot() ? "l" : "")
-				+ (hasRightRoot() ? "r" : "");
-		}
 	}
 }
