6667aa5ad9b4a8a3b065568155de548e3ef4d963
Igor Wiedler <igor@wiedler.ch>
2010-04-22 15:14:36 +0200

    re-use discriminant in quadraticfinder

--- src/finders/QuadraticFinder.java
+++ src/finders/QuadraticFinder.java
@@ -25,16 +25,16 @@ public class QuadraticFinder implements FinderInterface {
 			// two solutions
 			
 			// (-b +- sqrt(b^2-4ac)) / 2a
-			double x1 = (double) ((-b + Math.sqrt(Math.pow(b, 2)-4*a*c)) / (2*a));
+			double x1 = (double) ((-b + Math.sqrt(discriminant)) / (2*a));
 			results.add(x1);
-			double x2 = (double) ((-b - Math.sqrt(Math.pow(b, 2)-4*a*c)) / (2*a));
+			double x2 = (double) ((-b - Math.sqrt(discriminant)) / (2*a));
 			results.add(x2);
 			
 		} else if (discriminant == 0.0) {
 			// one solution
 			
 			// (-b + sqrt(b^2-4ac)) / 2a
-			double x1 = (double) ((-b + Math.sqrt(Math.pow(b, 2)-4*a*c)) / (2*a));
+			double x1 = (double) ((-b + Math.sqrt(discriminant)) / (2*a));
 			results.add(x1);
 			
 		} else /* if (discriminant < 0.0) */ {
